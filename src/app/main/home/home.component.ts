9087e0d648b3c538754c885c95292bc396fab5a4
Tanner Driggers <tannerdriggers@ksu.edu>
2019-02-04 15:47:54 -0600

    changed elementRef to renderer2

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { RedirectService } from 'src/app/redirect.service';
 
 @Component({
@@ -8,14 +8,14 @@ import { RedirectService } from 'src/app/redirect.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private redirect: RedirectService, private elementRef: ElementRef) { }
+  constructor(private redirect: RedirectService, private renderer: Renderer2) { }
 
   ngOnInit() {
     this.backgroundImage("modern-desk-dark-optimized.jpg");
   }
 
   backgroundImage(imageName: string) {
-    this.elementRef.nativeElement.ownerDocument.body.style.background = `url('./assets/${imageName}') center center fixed`;
+    this.renderer.setStyle(document.body, 'background', `url('./assets/${imageName}') center center fixed`);
   }
 
 }
